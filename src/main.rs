c9948c8b0168f1506ddeb8d6fd5d1feacdbb658d
Sebastian Pfeiler <seb.pfeiler@gmail.com>
2025-08-05 13:55:12 +0200

    allocator size changing

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,22 @@ use std::{
 };
 
 fn main() {
-	test_btset::<i16>();
-	test_btset::<i32>();
-	test_btset::<i64>();
+	test_btset::<i16, 0>();
+	test_btset::<i32, 0>();
+	test_btset::<i64, 0>();
+	test_btset::<i16, 32>();
+	test_btset::<i32, 32>();
+	test_btset::<i64, 32>();
+	test_btset::<i16, 128>();
+	test_btset::<i32, 128>();
+	test_btset::<i64, 128>();
 }
 
-fn test_btset<T : std::fmt::Display + std::cmp::Ord + From<i16>>() {
-	let ta = TrackingAllocator {};
-	println!("BTreeSet::<{}>", std::any::type_name::<T>());
-	let mut btset = BTreeSet::<T, TrackingAllocator>::new_in(ta);
-	for i in 0..1000 {
+fn test_btset<T : std::fmt::Display + std::cmp::Ord + From<i16>, const AllocatorSize : usize>() {
+	let ta = TrackingAllocator::<AllocatorSize> {_unused : [0; AllocatorSize]};
+	println!("BTreeSet::<{}> AllocatorSize {}", std::any::type_name::<T>(), AllocatorSize);
+	let mut btset = BTreeSet::<T, _>::new_in(ta);
+	for i in 0..20 {
 		btset.insert(i.into());
 	}
 	for elem in btset {
@@ -29,7 +35,9 @@ fn test_btset<T : std::fmt::Display + std::cmp::Ord + From<i16>>() {
 
 
 #[derive(Clone)]
-struct TrackingAllocator {}
+struct TrackingAllocator<const i : usize> {
+	_unused : [u8; i],
+}
 
 use std::{
 	alloc::{
@@ -44,7 +52,7 @@ use std::{
 		null,
 	},
 };
-unsafe impl Allocator for TrackingAllocator {
+unsafe impl<const i : usize > Allocator for TrackingAllocator<i> {
 	fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError>
 	{
 		println!("Allocating {:?}", layout);
